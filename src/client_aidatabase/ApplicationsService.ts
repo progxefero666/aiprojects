b22dfa7ca021fbfb02b7983348c6c11c94a4044f
progxefero666 <correoxefero@gmail.com>
2025-06-18 19:28:02 +0200

    dfsd

--- src/client_aidatabase/ApplicationsService.ts
+++ src/client_aidatabase/ApplicationsService.ts
@@ -10,42 +10,28 @@ import type { Application } from '@/client/models/Application';
  */
 export class ApplicationsService {
 
-    /**
-     * Getall
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
+    // read funciontions
     public static getAll(): CancelablePromise<Array<Application>> {
+        
         return __request(OpenAPI, {
-            method: 'GET',
+            method:     'GET',
             url: '/api/applications/',
         });
     }
 
-    /**
-     * Create
-     * @param requestBody
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
-    public static insert(requestBody:Application):CancelablePromise<Application> {
+    public static getByName(name:string):CancelablePromise<Array<Application>> {
         return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/applications/',
-            body: requestBody,
-            mediaType: 'application/json',
+            method: 'GET',
+            url: '/api/applications/name/{name}',
+            path: {
+                'name': name,
+            },
             errors: {
                 422: `Validation Error`,
             },
         });
     }
 
-    /**
-     * Get
-     * @param id
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
     public static getById(id:number):CancelablePromise<Application> {
         return __request(OpenAPI, {
             method: 'GET',
@@ -59,13 +45,58 @@ export class ApplicationsService {
         });
     }
 
-    /**
-     * Update
-     * @param id
-     * @param requestBody
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
+    public static getByProglanguage(proglanguage:string):CancelablePromise<Array<Application>> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/applications/proglanguage/{proglanguage}',
+            path: {
+                'proglanguage': proglanguage,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+    public static getByType(apptype: string): CancelablePromise<Array<Application>> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/applications/apptype/{apptype}',
+            path: {
+                'apptype': apptype,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+    public static getByUseDocker(usedocker:boolean): CancelablePromise<Array<Application>> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/applications/usedocker/{usedocker}',
+            path: {
+                'usedocker': usedocker,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+
+    public static insert(requestBody:Application):CancelablePromise<Application> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/applications/',
+            body: requestBody,
+            mediaType: 'application/json',
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
     public static update(id: number,requestBody:Application):CancelablePromise<Application> {
         return __request(OpenAPI, {
             method: 'PUT',
@@ -81,12 +112,6 @@ export class ApplicationsService {
         });
     }
     
-    /**
-     * Delete
-     * @param id
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
     public static delete(id:number):CancelablePromise<Application> {
         return __request(OpenAPI, {
             method: 'DELETE',
@@ -100,80 +125,10 @@ export class ApplicationsService {
         });
     }
 
-    /**
-     * Getbyapptype
-     * @param apptype
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
-    public static getByType(apptype: string): CancelablePromise<Array<Application>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/applications/apptype/{apptype}',
-            path: {
-                'apptype': apptype,
-            },
-            errors: {
-                422: `Validation Error`,
-            },
-        });
-    }
 
-    /**
-     * Getbyproglanguage
-     * @param proglanguage
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
-    public static getByProglanguage(proglanguage:string):CancelablePromise<Array<Application>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/applications/proglanguage/{proglanguage}',
-            path: {
-                'proglanguage': proglanguage,
-            },
-            errors: {
-                422: `Validation Error`,
-            },
-        });
-    }
 
-    /**
-     * Getbyusedocker
-     * @param usedocker
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
-    public static getByUseDocker(usedocker:boolean): CancelablePromise<Array<Application>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/applications/usedocker/{usedocker}',
-            path: {
-                'usedocker': usedocker,
-            },
-            errors: {
-                422: `Validation Error`,
-            },
-        });
-    }
+
+
     
-    /**
-     * Getbyname
-     * @param name
-     * @returns Application Successful Response
-     * @throws ApiError
-     */
-    public static getByName(name:string):CancelablePromise<Array<Application>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/applications/name/{name}',
-            path: {
-                'name': name,
-            },
-            errors: {
-                422: `Validation Error`,
-            },
-        });
-    }
 
 }//end service
